40e04f6dff640d0974a709b1fc1b1ec38737f5a5
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-21 05:23:56 +0530

    add main function to print user details including age, name, and height

---
+++ hello-world/print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 30
+	var name string = "MN Raza"
+	height := 5.5
+
+	fmt.Println("Age :", age, "Name: ", name, "Height :", height)
+	fmt.Printf("Hello, World!")
+	fmt.Println("Hello, World!")
+}
