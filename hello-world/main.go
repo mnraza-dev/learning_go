4e501a0d077e1bae7e07afc7687a228ec814fa53
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-21 05:12:09 +0530

    enhance main function to greet user by name and display version

--- hello-world/main.go
+++ hello-world/main.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	fmt.Println("Hello, World from main package!")
 	myutil.Hello("Hello, World!")
+
+	var name string = "John Doe"
+	var version = "1.0.0"
+	fmt.Println("Hello, " + name + "! Version: " + version)
 }
