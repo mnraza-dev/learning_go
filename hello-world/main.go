247481d6c9f83d152c3d76ff3aed6dc5e39b5ef7
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-21 05:18:43 +0530

    enhance main function with additional variables and print statements

--- hello-world/main.go
+++ hello-world/main.go
@@ -10,6 +10,24 @@ func main() {
 	myutil.Hello("Hello, World!")
 
 	var name string = "John Doe"
-	var version = "1.0.0"
+	var version string = "1.0.0"
+	var money int = 61200
+	var isSelected bool = true
+	const PI = 3.14
+
+	fmt.Println(PI)
+	fmt.Println(isSelected)
+	fmt.Println(money)
 	fmt.Println("Hello, " + name + "! Version: " + version)
+
+	// without using var and const
+	person := "Jane Doe"
+	fmt.Println("Hello, " + person + "!")
+
+	var Public = "Data is imp"           // can be used outside the package
+	var private = "Data is very private" // used only inside the package
+	fmt.Println(Public)
+	fmt.Println(private)
+
+	
 }
