b23ebe9fb1471f4bc32481676399d666bb862e17
Md Noorullah Raza <mnraza94@gmail.com>
2025-11-23 23:41:25 +0530

    add array example in arrays.go to demonstrate array initialization and usage

--- 08_arrays/arrays.go
+++ 08_arrays/arrays.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	var nums [4]int
+	nums[0] = 10
+	fmt.Println("Array:", nums)
+	fmt.Println("Array:", len(nums))
+	fmt.Println("First element:", nums[0])
 }
