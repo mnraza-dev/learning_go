f19def29cf34ebd0b74f4f4150821c54f94ed10b
Md Noorullah Raza <mnraza94@gmail.com>
2025-11-23 23:14:29 +0530

    loops using for

--- 05_for/for.go
+++ 05_for/for.go
@@ -1,8 +1,45 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	//for loop
-	
+	//for loop -> only construct in go for iteration
 
+	// while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i += 1
+	// }
+
+	// infinite loop
+	// for{
+	// 	fmt.Println("Hello, Go developers!")
+	// }
+
+	// traditional for loop
+	// for i := 1; i <= 3; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// continue statement
+	// for i := 1; i <= 3; i++ {
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+	// break statement
+	// for i := 1; i <= 3; i++ {
+	// 	if i == 2 {
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// range keyword
+	// for i := range 11 {
+	// 	fmt.Println(i)
+	// }
 }
