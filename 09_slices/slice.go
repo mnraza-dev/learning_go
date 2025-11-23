c26019b7120c5a775fec0dd04a2ac088d5285f79
Md Noorullah Raza <mnraza94@gmail.com>
2025-11-24 00:04:22 +0530

    slice with size 0

--- 09_slices/slice.go
+++ 09_slices/slice.go
@@ -13,19 +13,14 @@ func main() {
 	// fmt.Println(nums == nil)
 	// fmt.Println("Length:", len(nums))
 
-	var nums = make([]int, 5, 6) // initialize slice with size 5
+	// var nums = make([]int, 0, 6) // initialize slice with size 5
+	nums := []int{} // initialize empty slice
 	fmt.Println("Slice:", nums)
 	fmt.Println("Length:", len(nums))
 	fmt.Println("Capacity", cap(nums))
 	nums = append(nums, 10)
 	nums = append(nums, 23)
 	nums = append(nums, 67)
-	nums = append(nums, 234)
-	nums = append(nums, 674)
-	nums = append(nums, 637)
-	nums = append(nums, 6743)
-	nums = append(nums, 6738)
-	nums = append(nums, 6373)
 	fmt.Println("Slice after append:", nums)
 	fmt.Println("Length after append:", len(nums))
 	fmt.Println("Capacity after append:", cap(nums))
